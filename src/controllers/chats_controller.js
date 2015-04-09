f0606b99cd2690c56fec562aaa68527c8d353d17
fjl716 <fjl716@163.com>
2015-04-09 20:36:17 +0800

    update getUser

---
+++ src/controllers/chats_controller.js
@@ -0,0 +1,27 @@
+var MongoApi = require("../modules/mongoapi")
+var MongoController = MongoApi.Controller
+var ModelDefault = MongoApi.ModelDefault
+
+module.exports = new MongoController({
+  table: "chats",
+  model: {
+    Default: {
+      _id: ModelDefault.id,
+      create_at: ModelDefault.now,
+      from:ModelDefault.author
+    }
+  },
+  url: {
+    getChats: function (req, res, next) {
+      var model = {
+        query : {"$or": [{"form":{id:user._id}}, {"to":{id:user._id}}]}
+      };
+      this.DB.find(model, function (err, docs, next) {
+        var result = this.outFormat(docs);
+        res.send(result);
+        next();
+      }.bind(this));
+    }
+
+  }
+})
