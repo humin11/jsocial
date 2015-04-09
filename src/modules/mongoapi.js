677681e7f85f2f73248d3b640bf5b70f1b23b3db
fjl716 <fjl716@163.com>
2015-04-09 09:38:55 +0800

    edit mongoapi bug

--- src/modules/mongoapi.js
+++ src/modules/mongoapi.js
@@ -47,7 +47,7 @@ var MongoApi = {
     this.model.Default = (this.model.Default) ? this.model.Default : {};
     this.model.OutFormat = (this.model.OutFormat) ? this.model.OutFormat : {};
     this.model.OutFormat.apply = (this.model.OutFormat.apply) ? this.model.OutFormat.apply : {};
-    this.SimpleFormat = (params.SimpleFormat) ? params.SimpleFormat : this.SimpleFormat;
+    this.SimpleFormat = (params.SimpleFormat) ? params.SimpleFormat : MongoApi.SimpleFormat;
     var hide = {};
     if (this.model.OutFormat.hide) {
       for (var i = 0; i < this.model.OutFormat.hide.length; i++) {
