12261e871a0a25f93e90f033dab83239075888f6
StevenKwok <guozhaolong@gmail.com>
2015-04-13 11:34:08 +0800

    update session store with connect-mongo

--- config/express.js
+++ config/express.js
@@ -7,12 +7,21 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var expressBeautify = require('express-beautify')();
 var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
 
 module.exports = function(app, passport) {
   app.use(compression());
   app.use(cookieParser());
   app.use(bodyParser.json());
-  app.use(session({ secret: 'jsocial', resave: false, saveUninitialized: true, cookie: { maxAge: 60000 }}));
+  app.use(session({
+    secret: 'jsocial',
+    saveUninitialized: true, // don't create session until something stored
+    resave: false, //don't save session if unmodified
+    store: new MongoStore({
+      url: require(process.cwd() + '/config/database').url,
+      ttl: 14 * 24 * 60 * 60 // = 14 days. Default
+    })
+  }));
   app.use(passport.initialize());
   app.use(passport.session());
   app.use(require('express').static(path.join(process.cwd(), 'public')));
