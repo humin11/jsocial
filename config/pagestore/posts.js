60e1830313b044af0a3822a8f8f724bb16692177
StevenKwok <guozhaolong@gmail.com>
2015-04-17 21:00:23 +0800

    update

--- config/pagestore/posts.js
+++ config/pagestore/posts.js
@@ -5,18 +5,20 @@ var MongoApi = require('../../src/modules/mongoapi');
 var PostsController = require("../../src/controllers/posts_controller");
 var PostsModel= require("../../src/views/models/posts_model");
 var UserModel = require("../../src/views/models/user_model");
+var RecommendModel = require("../../src/views/models/recommend_model");
 var PageStore = require("./page_store");
 
 module.exports = function(Handler,req,sender) {
   PostsController.DB.find({index:1,count:20,query:{}}, function (err, obj, next) {
     var posts = new PostsModel();
     var user = new UserModel();
+    var recommend = new RecommendModel();
     user.set(req.user);
     posts.set(obj);
     next();
 
 
 
-    PageStore(sender,req,Handler,{PostsStore:obj,UsersStore:req.user},{posts:posts,user:user});
+    PageStore(sender,req,Handler,{PostsStore:obj,UsersStore:req.user},{posts:posts,user:user,recommend:recommend});
   });
 }
