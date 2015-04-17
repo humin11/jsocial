6363af9e4249dc4d33d6223242825880e1895b11
fjl716 <fjl716@163.com>
2015-04-17 16:10:51 +0800

    add static page store

--- config/pagestore/posts.js
+++ config/pagestore/posts.js
@@ -4,13 +4,15 @@
 var MongoApi = require('../../src/modules/mongoapi');
 var PostsController = require("../../src/controllers/posts_controller");
 var PostsModel= require("../../src/views/models/posts_model");
+var UserModel = require("../../src/views/models/user_model");
 var PageStore = require("./page_store");
 
 module.exports = function(Handler,req,sender) {
   PostsController.DB.find({index:1,count:20,query:{}}, function (err, obj, next) {
     var posts = new PostsModel();
+    var user = new UserModel();
     posts.set(obj);
     next();
-    PageStore(sender,req,Handler,{PostsStore:obj},{posts:posts});
+    PageStore(sender,req,Handler,{PostsStore:obj},{posts:posts,user:user});
   });
 }
