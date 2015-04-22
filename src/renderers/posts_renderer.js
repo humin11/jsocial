3edf3f66928dd98d174d583844d932a2346b86b2
fjl716 <fjl716@163.com>
2015-04-22 16:35:53 +0800

    update routers

--- src/renderers/posts_renderer.js
+++ src/renderers/posts_renderer.js
@@ -10,25 +10,40 @@ var RecommendModel = require("../models/recommend_model");
 var PageStore = require("./page_renderer");
 
 module.exports = function(Handler,req,sender) {
-  PostsController.DB.find({index:1,count:20,query:{}}, function (err, obj1, next1) {
-    var posts = new PostsModel();
-    var user = new UserModel();
-    var recommend = new RecommendModel();
-
-    if (req.user){
-      UsersController.DB.findOne({_id: MongoApi.ObjectId(req.user._id)},function(err,obj2,next2){
-        user.set(obj2);
-        next2();
-
-        posts.set(obj1);
-        next1();
-        PageStore(sender,req,Handler,{PostsStore:posts.get(),UsersStore:user.get()},{posts:posts,user:user,recommend:recommend});
+  var postsmodel = new PostsModel();
+  var usermodel = new UserModel();
+  var recommendmodel = new RecommendModel();
+  if (req.user) {
+    console.log(req.user);
+    UsersController.DB.findOne({_id: MongoApi.ObjectId(req.user._id)}, function (err, user, next2) {
+      usermodel.set(user);
+      next2();
+      var list = [];
+      user.followed.forEach(function(follow){
+        list.push(MongoApi.ObjectId(follow._id));
       });
-    }
-    else{
-      posts.set(obj1);
+      console.log(list);
+      PostsController.DB.find({query:{'author._id':{$in:list}},index: 1, count: 20}, function (err, posts, next1) {
+        console.log(posts);
+        postsmodel.set(posts);
+        next1();
+        PageStore(sender, req, Handler, {PostsStore: postsmodel.get(), UsersStore: usermodel.get()}, {
+          posts: postsmodel,
+          user: usermodel,
+          recommend: recommendmodel
+        });
+      });
+    });
+  }
+  else {
+    PostsController.DB.find({index: 1, count: 20, query: {}}, function (err, posts, next1) {
+      postsmodel.set(posts);
       next1();
-      PageStore(sender,req,Handler,{PostsStore:posts.get(),UsersStore:user.get()},{posts:posts,user:user,recommend:recommend});
-    }
-  });
+      PageStore(sender, req, Handler, {PostsStore: postsmodel.get(), UsersStore: usermodel.get()}, {
+        posts: postsmodel,
+        user: usermodel,
+        recommend: recommendmodel
+      });
+    });
+  }
 }
